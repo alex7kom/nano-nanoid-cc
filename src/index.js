ad8e9e4c8124dbaf852ca344d276f61967ddadce
Alexey Komarov <alex7kom@gmail.com>
2018-02-11 14:48:36 +0300

    remove negated condition

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ function calculate() {
     size: Number(sizeInput.value),
     alphabet: alphabetInput.value,
     uniqAlphabet: alphabetInput.value.split('').reduce(function(res, letter) {
-      return res.indexOf(letter) !== -1 ? res : res + letter;
+      return res.indexOf(letter) === -1 ? res + letter : res;
     }, ''),
     speed:
       speedUnitInput.value === 'hour'
