7b62fd81dab094e57e7eef7dadb74c5ad1ff3f0e
Alexey Komarov <alex7kom@gmail.com>
2018-02-11 15:39:14 +0300

    add more names of large numbers

--- src/util/duration.js
+++ src/util/duration.js
@@ -11,7 +11,14 @@ var timeNames = [
   { n: ['thousand years', 'thousands of years'], m: 10 },
   { n: ['million years', 'millions of years'], m: 1000 },
   { n: ['billion years', 'billions of years'], m: 1000 },
-  { n: ['trillion years', 'trillions of years'], m: 1000 }
+  { n: ['trillion years', 'trillions of years'], m: 1000 },
+  { n: ['quadrillion years', 'quadrillions of years'], m: 1000 },
+  { n: ['quintillion years', 'quintillions of years'], m: 1000 },
+  { n: ['sextillion years', 'sextillions of years'], m: 1000 },
+  { n: ['septillion years', 'septillions of years'], m: 1000 },
+  { n: ['octillion years', 'octillions of years'], m: 1000 },
+  { n: ['nonillion years', 'nonillions of years'], m: 1000 },
+  { n: ['decillion years', 'decillions of years'], m: 1000 }
 ];
 
 function formatDuration(seconds) {
